f82ecbc8ad862541fbec3f3fc6e5cf82707e7651
Caitlin <caitlinchou@gmail.com>
2018-07-03 12:15:12 -0400

    add author uri

--- move-email-to-top.php
+++ move-email-to-top.php
@@ -5,6 +5,7 @@ Plugin Name: Move Email to Top of WooCommerce Checkout
 Description: Moves email field to the top of the checkout to capture email early.
 Version: 1.0
 Author: Caitlin Chou
+Author URI: caitlinchou.me
 License: GPL3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 WC tested up to: 3.4.3
